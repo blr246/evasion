363427ebc30b332485e99fe9c3385b2f7cd7d93c
Akash Mehra <mehra.akash@gmail.com>
2011-11-01 15:11:57 -0400

    Added conditions for counter Diagonal in Hunter's Play Function. TODO: add distance check and return Hunter's move

--- hunter.h
+++ hunter.h
@@ -19,53 +19,88 @@ struct BasicHunter : public Hunter{
     // and is about to pass the prey (no matter how P moves)
     // then lay a wall to reduce the space available to P
     // if we can.
+    
+    
     State::HunterMotionInfo h = game.motionH;
     StepH move;
     move.wallCreateFlag = StepH::WallCreate_None;
     const State::Position& pDestination = game.posStackP.back();
     const State::Position& pSource = game.posStackP.at(game.posStackP.size()-1);
-    
-    int numerator = pDestination.y - pSource.y; // numerator in the slope of line passing through
-    int denominator = pDestination.x - pSource.x; // pDestination and pSource. 
-    //slope would be: numerator/denominator = (y2-y1)/(x2-x1)
-    
+    ComputeWallDirection(h,pDestination,pSource,&move);
     
     int wallCreationAllowedIn = -1;
     bool wallCreationForbidden = WallCreationLockedOut(game, &wallCreationAllowedIn);
-
-    Directions dirP = GetPreyDirection(numerator,denominator);
-    if(h.dir.x == 1 && h.dir.y == 1)
-    {
-        if((North == dirP || NorthEast == dirP))
-        {
-            move.wallCreateFlag = StepH::WallCreate_Horizontal;
-        }
-        else if(West == dirP || NorthWest == dirP)
-        {
-            move.wallCreateFlag = StepH::WallCreate_Vertical;
-        }
-    }
-    else if(h.dir.x == -1 && h.dir.y == -1)
-    {
-        if((South == dirP || SouthWest == dirP))
-        {
-            move.wallCreateFlag = StepH::WallCreate_Horizontal;
-        }
-        else if(East == dirP || SouthEast == dirP)
-        {
-            move.wallCreateFlag = StepH::WallCreate_Vertical;
-        }
-    }
-    //counter diagonal condition is missing, confirm with brandon to add that condition.
+        
     DoPly(move, &game);
     move.wallCreateFlag = StepH::WallCreate_None;
     DoPly(move, &game);
   }
     
+  void ComputeWallDirection(const State::HunterMotionInfo& h, 
+                            const State::Position& pDestination, 
+                            const State::Position& pSource, 
+                            StepH* move)
+  {
+    int numerator = pDestination.y - pSource.y; // numerator in the slope of line passing through
+    int denominator = pDestination.x - pSource.x; // pDestination and pSource. 
+    //slope would be: numerator/denominator = (y2-y1)/(x2-x1)
+
+    Directions dirP = GetPreyDirection(numerator,denominator);
+    if(dirP != Still)
+    {
+        if(h.dir.x == 1 && h.dir.y == 1)
+        {
+            if((North == dirP || NorthEast == dirP))
+            {
+                move->wallCreateFlag = StepH::WallCreate_Horizontal;
+            }
+            else if(West == dirP || NorthWest == dirP)
+            {
+                move->wallCreateFlag = StepH::WallCreate_Vertical;
+            }
+        }
+        else if(h.dir.x == -1 && h.dir.y == -1)
+        {
+            if((South == dirP || SouthWest == dirP))
+            {
+                move->wallCreateFlag = StepH::WallCreate_Horizontal;
+            }
+            else if(East == dirP || SouthEast == dirP)
+            {
+                move->wallCreateFlag = StepH::WallCreate_Vertical;
+            }
+        }
+        else if(h.dir.x == -1 && h.dir.y == 1)
+        {
+            if(North == dirP || NorthWest == dirP)
+            {
+                move->wallCreateFlag = StepH::WallCreate_Horizontal;
+            }//Horizontal
+            
+            else if(East == dirP || NorthEast == dirP)
+            {
+                move->wallCreateFlag = StepH::WallCreate_Vertical;
+            }//Vertical
+        }
+        else if(h.dir.x ==1 && h.dir.y == -1)
+        {
+            if(West == dirP || NorthWest == dirP)
+            {
+                move->wallCreateFlag = StepH::WallCreate_Vertical;
+            }// Vertical
+            if(South == dirP || SouthWest == dirP)
+            {
+                move->wallCreateFlag = StepH::WallCreate_Horizontal;
+            }
+        }
+    }
+
+  }
+    
   Directions GetPreyDirection(int numerator, int denominator)
   {
     Directions dirP = Still;
-    // gives the direction pf motion of prey.
+    // gives the direction of motion of prey.
     if(numerator < 0 && denominator > 0 )
     {
         dirP = NorthEast;
@@ -98,7 +133,7 @@ struct BasicHunter : public Hunter{
     {
         dirP = West;
     }
-    return dirP;
+    return dirP; // if none of the above is true then dirP = Still is returned.
   }
 
 };
